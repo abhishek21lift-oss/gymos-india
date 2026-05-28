c9e1e3d95494bd5758f966d352559bfc8a5bcab7
Abhishek Katiyar <abhishekatiyar@gmail.com>
2026-05-29 00:16:05 +0530

    Fix backend CI: add ESLint flat config, install typescript-eslint packages

---
+++ backend/eslint.config.js
@@ -0,0 +1,25 @@
+const tseslint = require('@typescript-eslint/eslint-plugin');
+const tsparser = require('@typescript-eslint/parser');
+
+module.exports = [
+  {
+    ignores: ['dist', 'node_modules'],
+  },
+  {
+    files: ['src/**/*.ts', 'test/**/*.ts'],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: {
+        project: './tsconfig.json',
+        tsconfigRootDir: __dirname,
+      },
+    },
+    plugins: {
+      '@typescript-eslint': tseslint,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '^_' }],
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  },
+];
